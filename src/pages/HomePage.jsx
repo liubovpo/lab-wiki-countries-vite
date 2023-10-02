c390eb491472eb97201badf90ef9c6423ed5f382
Eugen Freund <Eugen.Freund@outlook.de>
2023-10-02 14:03:53 +0200

    Init push

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,3 +1,27 @@
-function HomePage() {}
+import { useEffect, useState } from "react";
+
+function HomePage() {
+    const [countries, setCountries] = useState([]);
+
+    useEffect(()=> {
+        const getCountries = async() => {
+            const response = await fetch("https://ih-countries-api.herokuapp.com/countries")
+            const countriesAPI = await response.json();
+            setCountries(countriesAPI);
+        }
+        getCountries();
+        
+        console.log(countries);
+
+    }, [])
+
+    return (
+        <>
+            <h1>
+                WikiCountries: Your Guide to the World
+            </h1>
+        </>
+    )
+}
 
 export default HomePage;
