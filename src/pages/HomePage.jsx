e89952def3cacf8ce6cb00d17f701b502fd66bf7
liubovpo <102779079+liubovpo@users.noreply.github.com>
2023-10-02 14:40:53 +0200

    list

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import {Link} from 'react-router-dom'
 
 function HomePage() {
     const [countries, setCountries] = useState([]);
@@ -11,15 +12,24 @@ function HomePage() {
         }
         getCountries();
         
-        console.log(countries);
+        // console.log(countries);
 
     }, [])
-
     return (
         <>
             <h1>
                 WikiCountries: Your Guide to the World
             </h1>
+            <ul>
+                {countries.map((country) => (
+                    <Link key={country.alpha3Code} to={`/${country.alpha3Code}`}>
+                        <li style={{width:"80px"}}>
+                            <img src = {`https://flagpedia.net/data/flags/icon/72x54/${country.alpha2Code.toLowerCase()}.png`}  />
+                            {country.name.official}
+                        </li>
+                    </Link>
+                ))}
+            </ul>
         </>
     )
 }
